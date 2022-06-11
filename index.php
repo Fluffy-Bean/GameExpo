80f2d9bc2d1319fc1ad3c5b306987a24789cccf3
Fluffy-Bean <michal-gdula@protonmail.com>
2022-06-11 16:42:10 +0100

    Merge UI with index

--- index.php
+++ index.php
@@ -13,10 +13,21 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@600&display=swap">
 </head>
-	<?php
-		include"ui/navigationRoot.html";
-	?>
 	<body>
+		<!-- Navigation Root -->
+		<nav>
+			<img src="images/dv8.png" alt="DV8 logo" class="logo">
+			<input type="checkbox" id="toggler">
+			<label for="toggler"><i class="material-icons">menu</i></label>
+			<div class="menu">
+				<a href="#team" class="team">The Team</a>
+				<a href="#about" class="about">About</a>
+				<a href="#visitors" class="visitors">Visitors</a>
+				<a href="#times" class="times">Times</a>
+				<a href="#activities" class="activities">Activities</a>
+			</div>
+		</nav>
+
 		<!-- Root of all the text sections -->
 		<main>
 			<header>
@@ -25,14 +36,149 @@
 				<img src="images/wiggleAfter.png">
 			</header>
 
-			<!-- Root of sections -->
-
 			<!-- Teams -->
+			<!-- To professional web developers, please forgive me if I could've done all this better. -->
 			<section id="team">
 				<h2>The Teams</h2>
-				<?php
-				include"ui/teams.html";
-				?>
+				<!-- Foragers Haven -->
+				<div>
+				  <img src="images/teams/foragersHaven.png" alt="Foragers Haven logo" loading="lazy">
+				  <span></span>
+				  <div class="teamInfo">
+				    <h3>Foragers Haven</h3>
+				    <p>Team:</p>
+				    <p>- Farley Barker | 3D Environment Art, Game Design, Programming</p>
+				    <p>- Ben Easton | 3D Art, Story Development</p>
+				    <p>- Hannah Burfoot | Character Art, UI Design</p>
+				    <p>- Sasha Guerrier Gregory | Level Design, Game Design</p>
+				    <p>- Toby Pettman | Programming</p>
+				    <p>- Russell Charlwood | 3D Environment Art, Level Design, Programming </p>
+				    <p>About: Foragers Haven is a peaceful, cute, stylized adventure walking simulator. Explore a humble little forest, follow the quest line, craft potions and harvest plants, flowers and items around you.<p>
+				  </div>
+				</div>
+				<div class="gallery">
+				  <h3>Game screenshots</h3>
+				  <div>
+				    <img src="images/gallery/foragersHaven/1.png" loading="lazy">
+				    <img src="images/gallery/foragersHaven/2.png" loading="lazy">
+				    <img src="images/gallery/foragersHaven/3.png" loading="lazy">
+				    <img src="images/gallery/foragersHaven/4.png" loading="lazy">
+				    <img src="images/gallery/foragersHaven/5.png" loading="lazy">
+				    <img src="images/gallery/foragersHaven/6.png" loading="lazy">
+				  </div>
+				</div>
+
+				<!-- Spooked Mouse -->
+				<div>
+				  <img src="images/teams/spookedMouse.jpg" alt="Spooked Mouse logo" loading="lazy">
+				  <span></span>
+				  <div class="teamInfo">
+				    <h3>Mugunghwa</h3>
+				    <p>Team: Wren</p>
+				    <p>About: A game loosely set in South Korea about a lover who has been watching you from the shadows. They leave you gifts in hope that you'll one day seek them out so you can be together forever...</p>
+				  </div>
+				</div>
+				<div class="gallery">
+				  <h3>Game screenshots</h3>
+				  <div>
+				    <img src="images/gallery/mugunghwa/1.png" loading="lazy">
+				    <img src="images/gallery/mugunghwa/2.png" loading="lazy">
+				    <img src="images/gallery/mugunghwa/3.png" loading="lazy">
+				  </div>
+				</div>
+
+				<!-- RedScorpion -->
+				<div>
+				  <img src="images/teams/RSLogo.svg" alt="Red Scorpion logo" loading="lazy">
+				  <span></span>
+				  <div class="teamInfo">
+				    <h3>Again</h3>
+				    <p>Team: Leon, Bartek, Michal and Nathan</p>
+				    <p>About: An innovative game made towards gamers from all ends. Speedrunners and achievers, to casual gamers admiring works and the environment. With: high quality, custom made, assets; fast, fun gameplay and many ways to finish. You are surely going to have a fun time playing!</p>
+				  </div>
+				</div>
+				<div class="gallery">
+				  <h3>Game screenshots</h3>
+				  <div>
+				    <img src="images/gallery/again/1.png" loading="lazy">
+				    <img src="images/gallery/again/2.png" loading="lazy">
+				    <img src="images/gallery/again/3.png" loading="lazy">
+				    <img src="images/gallery/again/4.png" loading="lazy">
+				  </div>
+				</div>
+
+				<!-- Rainforest Runner -->
+				<div>
+				  <img src="images/teams/rainforestRunner.png" alt="Rainforrest Runner logo" loading="lazy">
+				  <span></span>
+				  <div class="teamInfo">
+				    <h3>Rainforest Runner</h3>
+				    <p>Team: Bradley, William, Munir and Sophie</p>
+				    <p>About: Rainforest Runner is an endless runner game where you will have to dodge the dangers of the Brazilian rainforest. If you survive, you're awarded with a chest full of treasure and rewards for completing the level. Good luck! You'll need it.</p>
+				  </div>
+				</div>
+				<div class="gallery">
+				  <h3>Game screenshots</h3>
+				  <div>
+				    <img src="images/gallery/rainforestRunner/1.png" loading="lazy">
+				    <img src="images/gallery/rainforestRunner/2.png" loading="lazy">
+				    <img src="images/gallery/rainforestRunner/3.png" loading="lazy">
+				    <img src="images/gallery/rainforestRunner/4.png" loading="lazy">
+				  </div>
+				</div>
+
+				<!-- Desperate Conflicts -->
+				<div>
+				  <img src="images/teams/desperateConflicts.png" alt="Desperate Conflicts logo" loading="lazy">
+				  <span></span>
+				  <div class="teamInfo">
+				    <h3>Desperate Conflicts</h3>
+				    <p>Team: Eliza Milburn and Yannick Fullager</p>
+				    <p>About: Desperate Conflicts is a fast paced real time strategy game, where you command fleets of ships in an attempt to destroy enemy bases via breaking through the increasingly difficult defences.</p>
+				  </div>
+				</div>
+				<div class="gallery">
+				  <h3>Game screenshots</h3>
+				  <div>
+				    <img src="images/gallery/desperateConflicts/1.png" loading="lazy">
+				    <img src="images/gallery/desperateConflicts/2.png" loading="lazy">
+				    <img src="images/gallery/desperateConflicts/3.png" loading="lazy">
+				  </div>
+				</div>
+
+				<!-- Foragers Haven -->
+				<div>
+				  <img src="images/teams/atrophy.png" alt="Atrophy logo" loading="lazy">
+				  <span></span>
+				  <div class="teamInfo">
+				    <h3>Atrophy</h3>
+				    <p>Team: Joseph, Sebastian</p>
+				  </div>
+				</div>
+				<div class="gallery">
+				  <h3>Game screenshots</h3>
+				  <div>
+				    <img src="images/gallery/atrophy/1.png" loading="lazy">
+				    <img src="images/gallery/atrophy/2.png" loading="lazy">
+				    <img src="images/gallery/atrophy/3.png" loading="lazy">
+				    <img src="images/gallery/atrophy/4.png" loading="lazy">
+				    <img src="images/gallery/atrophy/5.png" loading="lazy">
+				  </div>
+				</div>
+
+				<!-- More! -->
+				<div class="teamOther">
+				  <h3>And many others!</h3>
+				</div>
+
+				<!-- Screen overlay -->
+				<div class="fullScreenImage">
+				  <span>&times;</span>
+				  <img src="images/noImage.jpg" alt="">
+				</div>
+
+				<!-- Script -->
+				<script src="script.js"></script>
 			</section>
 
 			<section id="about">
@@ -95,10 +241,39 @@
 			</section>
 		</main>
 
+		<!-- Footer Root -->
+		<footer>
+			<div class="top">
+				<div class="links">
+					<h3>Social Links</h3>
+					<a href="https://twitter.com/dv8Expo/">Twitter</a>
+					<a href="https://www.instagram.com/dv8gameexpo/">Instagram</a>
+					<a href="https://discord.gg/xkHBS9hqGM">Discord</a>
+				</div>
+
+				<div class="contactInformation">
+					<h3>Contact Information</h3>
+					<p>DV8Expo@gmail.com</p>
+					<p>michal-gdula@pm.me</p>
+				</div>
+
+				<div class="locations">
+					<h3>Location</h3>
+					<p>Brighton</p>
+					<p>27 Richmond Pl</p>
+					<p>BN2 9NA</p>
+					<p></p>
+				</div>
+			</div>
+			<div class="uwu">
+				<p>Made by Michal</p>
+				<hr>
+				<a href="https://github.com/Fluffy-Bean/GameExpo">GitHub</a>
+				<hr>
+				<p>michal-gdula@pm.me</p>
+			</div>
+		</footer>
 	</body>
-	<?php
-		include"ui/footer.html";
-	?>
 	<!--
 	Hi! If you found this congrats, you have too much time, and this isnt the only thing that is hidden on this page!
 	But for now, have this ASCII maned wolf art :3-->
